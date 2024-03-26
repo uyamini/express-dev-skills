9b13b06a06fb41d9f35c226b360e23e796751043
uyamini <uyamini1996@gmail.com>
2024-03-26 13:22:24 -0500

    Added controller logic

---
+++ controllers/skills.js
@@ -0,0 +1,14 @@
+const Skill = require('../models/skill');
+
+function index(req, res) {
+  res.render('skills/index', { skills: Skill.getAll() });
+}
+
+function show(req, res) {
+  res.render('skills/show', { skill: Skill.getOne(req.params.id) });
+}
+
+module.exports = {
+  index,
+  show
+};
