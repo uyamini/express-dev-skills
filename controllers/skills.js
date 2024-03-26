6965f07735628505dde37973ba0ac687a45212a9
uyamini <uyamini1996@gmail.com>
2024-03-26 13:24:34 -0500

    Added skills router

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,11 +1,15 @@
 const Skill = require('../models/skill');
 
 function index(req, res) {
-  res.render('skills/index', { skills: Skill.getAll() });
+  res.render('skills/index', {
+     skills: Skill.getAll() 
+    });
 }
 
 function show(req, res) {
-  res.render('skills/show', { skill: Skill.getOne(req.params.id) });
+  res.render('skills/show', {
+     skill: Skill.getOne(req.params.id) 
+    });
 }
 
 module.exports = {
--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var skillsController = require('../controllers/skills');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// routes/skills.js
+
+router.get('/', skillsController.index);
+router.get('/:id', skillsController.show);
 
 module.exports = router;
